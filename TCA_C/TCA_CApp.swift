ec05cffed69a7fcc18ea9e2435d08680173b8bf6
nous178 <nous178@cashwalk.io>
2023-08-08 14:58:32 +0900

    Initial Commit

---
+++ TCA_C/TCA_CApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_CApp.swift
+//  TCA_C
+//
+//  Created by Taeuk on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct TCA_CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
