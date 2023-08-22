205da247d7b008ae97901851a8b0cead0a946666
HaneulKim <kim.haneul@cashwalk.io>
2023-08-22 17:14:32 +0900

    feat: 열 선택 기능 추가

--- TCA_C/View/RandomProfileRow.swift
+++ TCA_C/View/RandomProfileRow.swift
@@ -12,27 +12,52 @@ import Kingfisher
 struct RandomProfileRow: View {
     
     var profileInfo: RandomProfileData
+    var columnCount: Int
     
     var body: some View {
-        HStack(spacing: 10) {
-            KFImage(URL(string:profileInfo.picture.thumbnail)!)
-                .resizable()
-                .frame(width: 100, height: 100)
-                .aspectRatio(contentMode: .fit)
-                .clipped()
-                .padding(10)
-            VStack(alignment: .leading, spacing: 10) {
-                Text("\(profileInfo.name.title) \(profileInfo.name.first) \(profileInfo.name.last)")
-                    .font(.system(size: 20, weight: .bold))
-                Text(profileInfo.location.country)
-                    .font(.system(size: 20))
-                Text(profileInfo.email)
-                    .font(.system(size: 20))
-                    .lineLimit(1)
+        switch columnCount {
+        case 2:
+            VStack(spacing: 5) {
+                KFImage(URL(string: profileInfo.picture.thumbnail)!)
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .aspectRatio(contentMode: .fit)
+                    .clipped()
+                VStack(alignment: .leading, spacing: 3) {
+                    Text("\(profileInfo.name.title) \(profileInfo.name.first) \(profileInfo.name.last)")
+                        .font(.system(size: 20, weight: .bold))
+                    Text(profileInfo.location.country)
+                        .font(.system(size: 20))
+                    Text(profileInfo.email)
+                        .font(.system(size: 20))
+                        .lineLimit(nil)
+                }
             }
             .padding(10)
+            .frame(width: UIScreen.main.bounds.width / 2, height: 200)
+        default:
+            HStack {
+                KFImage(URL(string: profileInfo.picture.thumbnail)!)
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .aspectRatio(contentMode: .fit)
+                    .clipped()
+                VStack(alignment: .leading) {
+                    Text("\(profileInfo.name.title) \(profileInfo.name.first) \(profileInfo.name.last)")
+                        .font(.system(size: 20, weight: .bold))
+                        .lineLimit(nil)
+                        .padding(.bottom, 5)
+                    Text(profileInfo.location.country)
+                        .font(.system(size: 20))
+                    Text(profileInfo.email)
+                        .font(.system(size: 20))
+                        .lineLimit(nil)
+                }
+                .padding(10)
+                Spacer()
+            }
+            .frame(height: 100)
         }
-        
     }
 }
 
@@ -46,7 +71,7 @@ struct RandomProfileRow_Previews: PreviewProvider {
                 large: "https://randomuser.me/api/portraits/men/8.jpg",
                 medium: "https://randomuser.me/api/portraits/med/men/8.jpg",
                 thumbnail: "https://randomuser.me/api/portraits/thumb/men/8.jpg"
-            ))
+            )), columnCount: 2
         )
     }
 }
