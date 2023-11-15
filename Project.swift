c17ec325e165c924297141089dabac5efaaeb626
HaneulKim <khnpoq@naver.com>
2023-11-15 16:43:05 +0900

    update: tuist 적용

---
+++ Project.swift
@@ -0,0 +1,26 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project(
+  name: "TCA_C",
+  packages: Package.packages,
+  targets: [
+    Target(name: "TCA_C",
+           platform: .iOS,
+           product: .app,
+           bundleId: "com.TCA-C",
+           infoPlist: .default,
+           sources: ["TCA_C/Project/Source/**"],
+           resources: ["TCA_C/Project/Resource/**"],
+           dependencies: [
+            TargetDependency.Moya,
+            TargetDependency.CombineMoya,
+            TargetDependency.RxMoya,
+            TargetDependency.TCA,
+            TargetDependency.Kingfisher
+           ]
+          )
+  ],
+  schemes: [.Debug]
+)
+
